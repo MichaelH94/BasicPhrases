aeb1b1c51984a426c5e747746f20d528e93c86f1
zxcv1337x <michaelhaggard94@gmail.com>
2019-02-15 18:51:14 -0600

    finished app

--- app/src/main/java/com/example/basicphrases/MainActivity.java
+++ app/src/main/java/com/example/basicphrases/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.basicphrases;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void playPhrase(View view) {
+        Button buttonPressed = (Button) view;
+
+        Log.i("Button pressed", buttonPressed.getTag().toString());
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier(buttonPressed.getTag().toString(), "raw", getPackageName()));
+
+        mediaPlayer.start();
     }
 }
